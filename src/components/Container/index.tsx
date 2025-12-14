f332d1bf8c89434476ca8d20b95e47f77ce28c31
taciano <t.juniior13@gmail.com>
2025-12-14 12:31:57 -0300

    create component header and change container

--- src/components/Container/index.tsx
+++ src/components/Container/index.tsx
@@ -5,7 +5,7 @@ type ContainerProps = {
 export function Container({ children }: ContainerProps) {
   return (
     <div className="  text-slate-900 min-h-screen">
-      <div className=" bg-amber-500 max-w-lg mx-auto px-8">
+      <div className=" max-w-lg mx-auto px-8">
         <main>{children}</main>
       </div>
     </div>
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,9 @@
+export function Header() {
+  return (
+    <header>
+      <h1 className="text-6xl text-slate-800 font-bold py-8 text-center">
+        header
+      </h1>
+    </header>
+  );
+}
