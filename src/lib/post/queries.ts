df62389fd47ae57fa83f959c6b556ebd328b83cd
taciano <t.juniior13@gmail.com>
2025-12-25 11:51:17 -0300

    cache functions

--- src/lib/post/queries.ts
+++ src/lib/post/queries.ts
@@ -4,3 +4,9 @@ import { cache } from "react";
 export const findAllPublicPosts = cache(
   async () => await postRepository.findAllPublic()
 );
+export const findPostBySlug = cache(
+  async (slug: string) => await postRepository.findBySlug(slug)
+);
+export const findPostById = cache(
+  async (id: string) => await postRepository.findById(id)
+);
