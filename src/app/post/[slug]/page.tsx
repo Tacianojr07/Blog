6500026815bb3b56e0f0c4c05a335bafe4b3e8d5
taciano <t.juniior13@gmail.com>
2025-12-25 11:52:10 -0300

    using functions

--- src/app/post/[slug]/page.tsx
+++ src/app/post/[slug]/page.tsx
@@ -1,3 +1,6 @@
+import { postRepository } from "@/repositories/post";
+import { notFound } from "next/navigation";
+
 type ParamsProps = {
   params: Promise<{ slug: string }>;
 };
@@ -5,9 +8,18 @@ type ParamsProps = {
 export default async function PostSlugPage({ params }: ParamsProps) {
   const { slug } = await params;
 
+  let post;
+  try {
+    post = await postRepository.findBySlug(slug);
+  } catch {
+    post = undefined;
+  }
+
+  if (!post) notFound();
+
   return (
     <div>
-      <h1>rota dinamica {slug}</h1>
+      <h1>{post?.title}</h1>
     </div>
   );
 }
