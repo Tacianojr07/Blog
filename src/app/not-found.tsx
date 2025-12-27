147c181b296f44504d17d37a72abc57493a7d0c5
taciano <t.juniior13@gmail.com>
2025-12-27 15:23:38 -0300

    error message

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,15 +1,13 @@
+import { ErroMessage } from "@/components/ErrorMessage";
+
 export default function NotFound() {
   return (
     <>
-      <title>Página não encontrada</title>
-      <div className="min-h-[320px] bg-slate-900 text-slate-200 p-8 mb-16 rounded-xl flex items-center justify-center">
-        <div className="text-center">
-          <h1 className="text-7xl font-bold mb-4">404</h1>
-          <p className="text-xl">
-            Esta página que você tentou acessar não foi encontrada.
-          </p>
-        </div>
-      </div>
+      <ErroMessage
+        pageTitle="Página não encontrada"
+        content="ERROR 404 - Esta página que você tentou acessar não foi encontrada."
+        contentTitle="404"
+      />
     </>
   );
 }
