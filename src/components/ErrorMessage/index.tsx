adb482a35b7fba61bc64eb60f6a4d8d0cfd9ca8f
taciano <t.juniior13@gmail.com>
2025-12-27 15:22:59 -0300

    create new component error message

---
+++ src/components/ErrorMessage/index.tsx
@@ -0,0 +1,22 @@
+type ErrorMessageProps = {
+  pageTitle: string;
+  content: string;
+  contentTitle: string;
+};
+export function ErroMessage({
+  pageTitle,
+  content,
+  contentTitle,
+}: ErrorMessageProps) {
+  return (
+    <>
+      <title>{pageTitle}</title>
+      <div className="min-h-80 bg-slate-900 text-slate-200 p-8 mb-16 rounded-xl flex items-center justify-center">
+        <div className="text-center">
+          <h1 className="text-7xl font-bold mb-4">{contentTitle}</h1>
+          <div className="text-xl">{content}</div>
+        </div>
+      </div>
+    </>
+  );
+}
